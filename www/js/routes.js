f95053c3a5b9a38638f33ffd11528653c6215031
Steven Jackson <stevenjackson.sanguine@gmail.com>
2016-03-11 21:54:32 +0200

    App working. TODO: Entry form and Homepage

--- www/js/routes.js
+++ www/js/routes.js
@@ -13,7 +13,7 @@ angular.module('app.routes', [])
       .state('menu.home', {
     url: '/home',
     views: {
-      'side-menu21': {
+      'dairy': {
         templateUrl: 'templates/home.html',
         controller: 'homeCtrl'
       }
@@ -21,45 +21,69 @@ angular.module('app.routes', [])
   })
 
   .state('menu', {
-    url: '/side-menu21',
+    url: '/dairy',
     templateUrl: 'templates/menu.html',
     abstract:true
   })
 
-  .state('overview', {
+  .state('menu.overview', {
     url: '/overview',
-    templateUrl: 'templates/overview.html',
-    controller: 'overviewCtrl'
+    views: {
+      'dairy': {
+        templateUrl: 'templates/overview.html',
+        controller: 'overviewCtrl'
+      }
+    }
   })
 
-  .state('recentEntries', {
+  .state('menu.recentEntries', {
     url: '/recent-entries',
-    templateUrl: 'templates/recentEntries.html',
-    controller: 'recentEntriesCtrl'
+    views: {
+      'dairy': {
+        templateUrl: 'templates/recentEntries.html',
+        controller: 'recentEntriesCtrl'
+      }
+    }
   })
 
-  .state('archive', {
+  .state('menu.archive', {
     url: '/archive',
-    templateUrl: 'templates/archive.html',
-    controller: 'archiveCtrl'
+    views: {
+      'dairy': {
+        templateUrl: 'templates/archive.html',
+        controller: 'archiveCtrl'
+      }
+    }
   })
 
-  .state('search', {
+  .state('menu.search', {
     url: '/search',
-    templateUrl: 'templates/search.html',
-    controller: 'searchCtrl'
+    views: {
+      'dairy': {
+        templateUrl: 'templates/search.html',
+        controller: 'searchCtrl'
+      }
+    }
   })
 
-  .state('aboutMe', {
+  .state('menu.aboutMe', {
     url: '/about-me',
-    templateUrl: 'templates/aboutMe.html',
-    controller: 'aboutMeCtrl'
+    views: {
+      'dairy': {
+        templateUrl: 'templates/aboutMe.html',
+        controller: 'aboutMeCtrl'
+      }
+    }
   })
 
-  .state('contact', {
+  .state('menu.contact', {
     url: '/contact',
-    templateUrl: 'templates/contact.html',
-    controller: 'contactCtrl'
+    views: {
+      'dairy': {
+        templateUrl: 'templates/contact.html',
+        controller: 'contactCtrl'
+      }
+    }
   })
 
   .state('newEntry', {
@@ -68,7 +92,53 @@ angular.module('app.routes', [])
     controller: 'newEntryCtrl'
   })
 
-$urlRouterProvider.otherwise('/side-menu21/home')
+  .state('menu.archive2016', {
+    url: '/2016',
+    views: {
+      'dairy': {
+        templateUrl: 'templates/archive2016.html',
+        controller: 'archiveCtrl'
+      }
+    }
+  })
+
+.state('menu.archive2015', {
+    url: '/2015',
+    views: {
+      'dairy': {
+        templateUrl: 'templates/archive2015.html',
+        controller: 'archiveCtrl'
+      }
+    }
+  })
+
+
+.state('menu.archive2015month', {
+    url: '/archive/2015/:monthFilter',
+    views: {
+      'dairy': {
+        templateUrl: 'templates/archiveListing.html',
+        controller: 'archiveListingCtrl'
+       
+      }
+    }
+  })
+
+.state('menu.archive2016month', {
+    url: '/archive/2016/:monthFilter',
+    views: {
+      'dairy': {
+        templateUrl: 'templates/archiveListing.html',
+        controller: 'archiveListingCtrl'
+       
+      }
+    }
+  })
+
+
+
+
+$urlRouterProvider.otherwise('/dairy/home')
 
   
 
--- www/js/services.js
+++ www/js/services.js
@@ -6,5 +6,25 @@ angular.module('app.services', [])
 
 .service('BlankService', [function(){
 
-}]);
+}])
 
+// Create Invoice Factory object
+.factory('Task', function($http){
+
+	var taskFactory = {};
+
+// Create post factory to post data to api
+	taskFactory.create = function(taskData){
+
+		return	$http.post('http://boiling-mesa-13925.herokuapp.com/api/new-task',taskData)
+	}
+
+// Create get factory to retrieve data from api
+	taskFactory.all = function(){
+console.log("Successful GET")
+		return	$http.get('http://boiling-mesa-13925.herokuapp.com/api/tasks');
+	}
+
+	return	taskFactory;
+
+});
