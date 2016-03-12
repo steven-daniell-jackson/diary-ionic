a2d60d796c02761601053261289e31b5e1948f44
Steven Jackson <stevenjackson.sanguine@gmail.com>
2016-03-12 17:47:38 +0200

    Fixed form inpur error. Not clearing

--- js/controllers.js
+++ js/controllers.js
@@ -205,7 +205,61 @@ $scope.preloader = false;
 
 })
    
-.controller('newEntryCtrl', function($scope) {
+.controller('newEntryCtrl', function($scope, Task) {
+
+
+console.log($scope.newEntry)
+
+// Submit function bound on "Submit" button on form
+$scope.submit = function(form){
+
+
+// Check if form is valid
+if(form.$valid) {
+
+  $scope.workdone = '';
+
+
+// Debug: 
+console.log("Form valid")
+
+// Use Data-binding to build object to send to API
+var formData = {
+
+  platform: form.platform.$viewValue,
+  clientname: form.client.$viewValue,
+  url: form.url.$viewValue,
+  workdone: form.workdone.$viewValue,
+  date: form.date.$viewValue,
+  monthfilter: form.yearfilter.$viewValue,
+  yearfilter: form.monthfilter.$viewValue,
+  comments: form.comments.$viewValue
+
+}
+
+// Call factory and submit the formData for processing
+Task.create(formData)
+
+ // If successful print returned message form API else print error
+ .success(function(data) {
+
+// Clear form
+window.location.reload(); 
+
+
+
+// Data binding on status to log API returned message
+$scope.status = data.message;
+
+})
+ .error(function(data) {
+  console.log('Error: ' + data);
+ });
+
+
+}
+
+}
 
 })
  
